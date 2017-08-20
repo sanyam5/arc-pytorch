81f7e961b2d77e1b69f0a4557e047c1a6dbf8a82
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2017-08-20 22:59:04 +0530

    Print the train and validation accuracies as well

--- train.py
+++ train.py
@@ -1,8 +1,17 @@
 from models import Discriminator
 import torch
+from torch.autograd import Variable
 from datetime import datetime, timedelta
 
 from batcher import Batcher
+
+
+def get_pct_accuracy(pred: Variable, target) -> int:
+    hard_pred = (pred > 0.5).int()
+    correct = (hard_pred == target).sum().data[0]
+    accuracy = float(correct) / target.size()[0]
+    accuracy = int(accuracy * 100)
+    return accuracy
 
 
 def train():
@@ -13,7 +22,7 @@ def train():
     optimizer = torch.optim.Adam(params=disc.parameters(), lr=3e-4)
 
     best_validation_loss = None
-    saving_threshold = 1.2
+    saving_threshold = 1.02
     last_saved = None
     save_every = timedelta(minutes=10)
 
@@ -36,8 +45,8 @@ def train():
             training_loss = loss.data[0]
             validation_loss = loss_val.data[0]
 
-            print("Iteration: {} \t Training loss: {} \t Validation loss: {}".format(
-                i, training_loss, validation_loss
+            print("Iteration: {} \t Train({}%): {} \t Validation({}%): {}".format(
+                i, get_pct_accuracy(pred, Y), training_loss, get_pct_accuracy(pred_val, Y_val), validation_loss
             ))
 
             if best_validation_loss is None or best_validation_loss > (saving_threshold * validation_loss):
